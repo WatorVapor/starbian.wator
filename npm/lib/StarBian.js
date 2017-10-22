8c2be2c35a96e3331dc418ca3f7a929a4190488f
Wator Vapor <watorvapor@gmail.com>
2017-10-22 21:25:56 +0900

    Update StarBian.js

--- npm/lib/StarBian.js
+++ npm/lib/StarBian.js
@@ -25,10 +25,10 @@ class StarBian {
     }
     if(fs.existsSync('.keys/prv.pem')) {
       this.prvKeyStr = rsu.readFile('.keys/prv.pem');
-      //console.log('this.prvKeyStr=<',this.prvKeyStr,'.');
+      //console.log('this.prvKeyStr=<',this.prvKeyStr,'>');
       if(fs.existsSync('.keys/pub.pem')) {
         this.pubKeyStr = rsu.readFile('.keys/pub.pem');
-        //console.log('this.pubKeyStr=<',this.pubKeyStr,'.');
+        //console.log('this.pubKeyStr=<',this.pubKeyStr,'>');
       } else {
         this.reCreatePubKey();
       }
@@ -37,21 +37,15 @@ class StarBian {
     }
     this.priObj = rs.KEYUTIL.getKey(this.prvKeyStr);
     this.pubObj = rs.KEYUTIL.getKey(this.pubKeyStr);
-    //console.log('this.priObj=<',this.priObj,'.');
-    //console.log('this.pubObj=<',this.pubObj,'.');
-    this.clientPub = redis.createClient({
-      host:'www.wator.xyz',
-      port:6379
-    });
+    //console.log('this.priObj=<',this.priObj,'>');
+    //console.log('this.pubObj=<',this.pubObj,'>');
+    this.clientPub = redis.createClient({host:'www.wator.xyz'});
     this.clientPub.on('error', function (err) {
-      console.log('this.clientPub err=<',this.err,'.');
+      console.log('this.clientPub err=<',err,'>');
     });
-    this.clientSub = redis.createClient({
-      host:'www.wator.xyz',
-      port:6379
-    });
+    this.clientSub = redis.createClient({host:'www.wator.xyz'});
     this.clientSub.on('error', function (err) {
-      console.log('this.clientSub err=<',this.err,'.');
+      console.log('this.clientSub err=<',err,'>');
     });
   }
   /**

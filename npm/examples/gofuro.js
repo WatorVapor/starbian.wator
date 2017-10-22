6efb1f18e35989113dbef8428975632e8566c6cd
Wator Vapor <watorvapor@gmail.com>
2017-10-22 11:12:48 +0900

    Update gofuro.js

--- npm/examples/gofuro.js
+++ npm/examples/gofuro.js
@@ -3,3 +3,21 @@ const StarBian = require('..');
 
 var gofuro = new StarBian();
 console.log('gofuro=<',gofuro,'>');
+
+var private = gofuro.getPrivate();
+console.log('private=<',private,'>');
+
+var public  = gofuro.getPublic();
+console.log('public=<',public,'>');
+
+var authed  = gofuro.getAuthed();
+console.log('authed=<',authed,'>');
+
+
+gofuro.subscribe( function(msg) {
+  console.log('msg=<',msg,'>');
+});
+
+gofuro.publish('ok hot hot run');
+
+console.log('gofuro=<',gofuro,'>');

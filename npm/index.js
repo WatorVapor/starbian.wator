79299dfb988c71853ada2cb4065e9d728eac5e53
Wator Vapor <watorvapor@gmail.com>
2017-10-22 10:40:17 +0900

    Update index.js

--- npm/index.js
+++ npm/index.js
@@ -1,3 +1,8 @@
+/*!
+ * ws: a node.js star bian client
+ * Copyright(c) 2017 Wato Vapor <watovapor@gmail.com>
+ * MIT Licensed
+ */
 'use strict';
 const StarBian = require('./lib/StarBian');
 module.exports = StarBian;

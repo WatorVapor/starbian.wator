81d58b9f1b3c4907df4d256ae543f8793f2a5792
Wator Vapor <watorvapor@gmail.com>
2017-10-24 21:41:42 +0900

    Update app.js

--- ws/app.js
+++ ws/app.js
@@ -3,4 +3,9 @@
  * Copyright(c) 2017 Wato Vapor <watovapor@gmail.com>
  * MIT Licensed
  */
- 
+
+'use strict';
+const StarBian = require('starbian');
+
+var wsProxy = new StarBian();
+//console.log('wsProxy=<',wsProxy,'>');

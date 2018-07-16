ea83016819f9d994b221cf090568ad3e5655a42f
Wator Vapor <watorvapor@gmail.com>
2018-07-16 21:41:05 +0900

    Update app.js

--- ws/app.js
+++ ws/app.js
@@ -15,20 +15,38 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({host:'127.0.0.1', port: 19080 });
 
 let wsClients = {};
- 
+
+
+function noop() {};
+function heartbeat() {
+  this.isAlive = true;
+}
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 30000);
+
+
 wss.on('connection', function (ws) {
   console.log('ws=<', ws,'>');
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
   ws.on('message', function (message) {
     console.log('received: message=<', message,'>');
     try {
-      var jsonMsg = JSON.parse(message);
+      let jsonMsg = JSON.parse(message);
       console.log('jsonMsg=<', jsonMsg,'>');
       if(jsonMsg && jsonMsg.channel && jsonMsg.msg) {
-       wsProxy.passthrough(jsonMsg.channel,jsonMsg.msg);
+        wsProxy.passthrough(jsonMsg.channel,jsonMsg.msg);
       }
       if(jsonMsg && jsonMsg.channel && jsonMsg.subscribe) {
-       wsProxy.subscribe(jsonMsg.channel,onStarBianMsg);
-       wsClients[jsonMsg.channel] = ws;
+        wsProxy.subscribe(jsonMsg.channel,onStarBianMsg);
+        wsClients[jsonMsg.channel] = ws;
       }
     } catch(e){
       console.log('e=<', e,'>');

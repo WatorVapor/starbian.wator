896831dd067599284a1237c9cd713cf95d75bc0d
Wator Vapor <watorvapor@gmail.com>
2018-08-24 13:19:49 +0900

    Update gofuro.js

--- npm/examples/gofuro/gofuro.js
+++ npm/examples/gofuro/gofuro.js
@@ -16,22 +16,3 @@ console.log('authedKey=<',authedKey,'>');
 
 
 
-let listenChannel = authedKey[0];
-gofuro.subscribe(listenChannel,(channel,msg) =>{
-  console.log('channel=<',channel,'>');
-  console.log('msg=<',msg,'>');
-});
-
-gofuro.onReady = ()=> {
-  setTimeout(function(){
-    gofuro.publish('ok hot hot run');
-  },20000);
-};
-
-/*
-setTimeout(function(){
-  gofuro.publish('ok hot hot run');
-},2000);
-*/
-
-//console.log('gofuro=<',gofuro,'>');

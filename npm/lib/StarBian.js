2231f17620251c613d5e9e9ad742667c41560f06
Wator Vapor <watorvapor@gmail.com>
2018-08-26 15:48:56 +0900

    Update StarBian.js

--- npm/lib/StarBian.js
+++ npm/lib/StarBian.js
@@ -469,7 +469,7 @@ class StarBian {
       console.log('_encrypt enMsg=<' , enMsg , '>');
       let enObj = {
         iv:buf2hex(iv),
-        encrypt:ab2str(enMsg)
+        encrypt:ab2str(enMsg,'hex')
       };
       cb(enObj);
     })

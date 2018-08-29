108525219f02751d0158dde547121acccbe028d2
Wator Vapor <watorvapor@gmail.com>
2018-08-29 21:56:20 +0900

    Update and rename publishKey.js to broadcastPubKey.js

--- npm/bin/broadcastPubKey.js
+++ npm/bin/broadcastPubKey.js
@@ -2,10 +2,10 @@
 'use strict';
 const StarBian = require('..');
 
-let publishKey = new StarBian();
-//console.log('publishKey=<',publishKey,'>');
-publishKey.onReady = () => {
-  publishKey.broadcastPubKey( (password,progress,finnish) => {
+let broadcast = new StarBian();
+//console.log('broadcast=<',broadcast,'>');
+broadcast.onReady = () => {
+  broadcast.broadcastPubKey( (password,progress,finnish) => {
     console.log('please search this key by <',password,'> will finnish at <',progress,'seconds');
     if(finnish) {
       process.exit(0);

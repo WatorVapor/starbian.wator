1cde33aae7d785caf24fb8e12a5e8798eb94ef02
Wator Vapor <watorvapor@gmail.com>
2018-08-29 21:48:54 +0900

    Update and rename shareKey.js to publishKey.js

---
+++ npm/bin/publishKey.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+const StarBian = require('..');
+
+let publishKey = new StarBian();
+//console.log('publishKey=<',publishKey,'>');
+
+searchKey.onReady = () => {
+}

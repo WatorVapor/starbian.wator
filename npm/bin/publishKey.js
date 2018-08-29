6df3b294e328401e6354fff1f44fa31ae92920f5
Wator Vapor <watorvapor@gmail.com>
2018-08-29 21:55:22 +0900

    Update publishKey.js

--- npm/bin/publishKey.js
+++ npm/bin/publishKey.js
@@ -4,5 +4,11 @@ const StarBian = require('..');
 
 let publishKey = new StarBian();
 //console.log('publishKey=<',publishKey,'>');
-searchKey.onReady = () => {
+publishKey.onReady = () => {
+  publishKey.broadcastPubKey( (password,progress,finnish) => {
+    console.log('please search this key by <',password,'> will finnish at <',progress,'seconds');
+    if(finnish) {
+      process.exit(0);
+    }
+  });
 }

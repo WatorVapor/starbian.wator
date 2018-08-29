a7f817430c8f850c32b98ac323807f9d7bd2f73b
Wator Vapor <watorvapor@gmail.com>
2018-08-29 21:06:49 +0900

    Update searchKey.js

--- npm/bin/searchKey.js
+++ npm/bin/searchKey.js
@@ -1,1 +1,17 @@
+'use strict';
+const StarBian = require('..');
 
+let searchKey = new StarBian();
+//console.log('searchKey=<',searchKey,'>');
+
+searchKey.subscribe_broadcast((msg,channel) => {
+  //console.log('searchKey.subscribe_broadcast typeof msg=<',typeof msg,'>');
+  if(typeof msg === 'string') {
+    msg = JSON.parse(msg);
+  }
+  //console.log('searchKey.subscribe_broadcast msg=<',msg,'>');
+  //console.log('searchKey.subscribe_broadcast channel=<',channel,'>');
+  if(msg.shareKey) {
+    console.log('searchKey.subscribe_broadcast msg.shareKey=<',msg.shareKey,'>');
+  }
+});

04cae438287dda8df9c95fdffc69573b2e1b91f2
Wator Vapor <watorvapor@gmail.com>
2018-09-01 21:34:40 +0900

    Update run.sh

--- ws/run.sh
+++ ws/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find / | grep node_modules >./all.files
-export NODE_PATH=/usr/local/lib/node_modules
+export NODE_PATH=/usr/local/lib/node_modules:/usr/src/app/node_modules:$NODE_PATH
 node ./app.js

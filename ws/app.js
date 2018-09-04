2af281524253da488a79054eb85b9dd412d1358a
Wator Vapor <watorvapor@gmail.com>
2018-09-04 23:02:00 +0900

    Update app.js

--- ws/app.js
+++ ws/app.js
@@ -23,11 +23,13 @@ function heartbeat() {
 
 const interval = setInterval(function ping() {
   wss.clients.forEach(function each(ws) {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.ping(noop);
+    }
     if (ws.isAlive === false) {
       return ws.terminate();
     }
     ws.isAlive = false;
-    ws.ping(noop);
   });
 }, 30000);
 

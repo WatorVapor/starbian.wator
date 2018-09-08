e1be527ca406d4798ed4a922c295c3fed33e8e3e
Wator Vapor <watorvapor@gmail.com>
2018-09-08 20:59:38 +0900

    Create StarBianPeer.js

---
+++ npm/lib/StarBianPeer.js
@@ -0,0 +1,45 @@
+/*!
+ * starbian: a node.js star bian client
+ * Copyright(c) 2017 Wato Vapor <watovapor@gmail.com>
+ * MIT Licensed
+ */
+'use strict';
+
+const StarBianInner = require('./star_bian_inner');
+class StarBianPeer {
+  /**
+   * Create a new `StarBianPeer`.
+   *
+   */
+  constructor (keyChannel) {
+    this.inner_ = new StarBianInner();
+    this.keyChannel_ = keyChannel;
+    this.inner_.onReady = (priKey,pubKey,authedKey) => {
+      if(typeof this.onReady === 'function') {
+        this.onReady(priKey,pubKey,authedKey);
+      }
+    };
+  }
+  /**
+   * publish a messege.
+   *
+   * @param {String} msg 
+   * @param {String} channel 
+   */
+  publish(msg) {
+    this.inner_.publish(msg,this.keyChannel_);
+  }
+  /**
+   * subscribe.
+   *
+   * @param {Function} callback 
+   */
+  subscribe(callback) {
+    this.inner_.subscribe(callback);
+  }
+}
+
+
+module.exports = {
+  Peer:StarBianPeer
+};

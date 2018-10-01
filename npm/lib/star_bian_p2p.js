4cd9b4da0349882226bb0fdf7b66441cacb99bec
Wator Vapor <watorvapor@gmail.com>
2018-10-01 14:25:58 +0900

    Update star_bian_p2p.js

--- npm/lib/star_bian_p2p.js
+++ npm/lib/star_bian_p2p.js
@@ -20,7 +20,7 @@ const IPFS_CONF = {
   config: {
     Addresses: {
       Swarm: [
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        //'/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
       ]
     }
   }

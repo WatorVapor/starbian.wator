368d3eb63c5a0e744f44d89961dd1e438ec52c7b
Wator Vapor <watorvapor@gmail.com>
2018-10-08 05:29:43 +0000

    linux

---
+++ npm/examples/door.camera/app.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ host:'127.0.0.1',port: 18080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  //ws.send('something');
+});

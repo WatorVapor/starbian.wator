e46fe73121ee8bed0634f7ab96be7b709acde47b
Wator Vapor <watorvapor@gmail.com>
2018-10-08 14:34:03 +0900

    Update app.js

--- sample/door.camera/app.js
+++ sample/door.camera/app.js
@@ -4,7 +4,9 @@ const wss = new WebSocket.Server({ host:'127.0.0.1',port: 18080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log('message=<' , message , '>');
+    let msgJson = JSON.parse(message);
+    console.log('msgJson=<' , msgJson , '>');
   });
   //ws.send('something');
 });

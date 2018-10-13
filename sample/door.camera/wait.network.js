0f1de9a0cbbaeedfdc973a74c820fb2c487fe239
Wator Vapor <watorvapor@gmail.com>
2018-10-13 19:39:43 +0900

    Update wait.network.js

--- sample/door.camera/wait.network.js
+++ sample/door.camera/wait.network.js
@@ -2,7 +2,7 @@ const execSync = require("child_process").execSync;
 while(true) {
   try {
     let log = execSync('ping -c 1 www.wator.xyz');
-    console.log('log=<',log,'>');
+    //console.log('log=<',log,'>');
     break;
   } catch(e) {
     //console.error('e=<',e,'>');

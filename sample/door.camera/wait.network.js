1df7cf934b5cf20483116ca8063d0d67435dc384
Wator Vapor <watorvapor@gmail.com>
2018-10-15 23:28:54 +0900

    Update wait.network.js

--- sample/door.camera/wait.network.js
+++ sample/door.camera/wait.network.js
@@ -23,8 +23,8 @@ while(true) {
 }
 console.log('ok websocket is good');
 */
+const net = require('net');
 const client = net.createConnection({ host:'127.0.0.1',port: 18080 });
-
 client.on('connect', function(){
   console.log('client:connected');
 });

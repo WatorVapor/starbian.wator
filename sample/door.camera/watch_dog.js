9f2b40f07f0cdebbfb08098fe001536c5a5a2bf2
Wator Vapor <watorvapor@gmail.com>
2018-10-24 00:06:46 +0900

    Update watch_dog.js

--- sample/door.camera/watch_dog.js
+++ sample/door.camera/watch_dog.js
@@ -1,5 +1,5 @@
 const execSync = require("child_process").execSync;
-const kCheckInterval = 1000 * 60 *5;
+const kCheckInterval = 1000 * 60 *1;
 let badCounterPing = 0;
 let badCounterChromium = 0;
 let badCounterWS = 0;

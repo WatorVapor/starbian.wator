5f7375ecab75aab081a8e4b9229e03e0a6a41c92
Wator Vapor <watorvapor@gmail.com>
2018-10-27 14:12:01 +0900

    Update main.cpp

--- sample/door.camera/opencv/main.cpp
+++ sample/door.camera/opencv/main.cpp
@@ -1,6 +1,19 @@
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+#define DUMP_VAR(x)                                                            \
+  {                                                                            \
+    std::cout << __func__ << ":" << __LINE__ << "::" << #x << "=<" << x << ">" \
+              << std::endl;                                                    \
+  }
+
+#include "opencv2/opencv.hpp"
+
+const string cascade_name("/opt/local/share/opencv/haarcascades/haarcascade_frontalface_default.xml");
 
 int main (int argc, char **argv) {
+  cv::VideoCapture cap(0);
+  DUMP_VAR(cap.isOpened());
   return 0;
 }

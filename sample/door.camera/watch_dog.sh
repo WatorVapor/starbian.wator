cc312ba10e551f17a576f8a4e4f310f83c611708
Wator Vapor <watorvapor@gmail.com>
2018-10-27 14:00:23 +0900

    Update watch_dog.sh

--- sample/door.camera/watch_dog.sh
+++ sample/door.camera/watch_dog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p /tmp/door_camera
+mount -t tmpfs -o size=4M tmpfs /tmp/door_camera
 log=/tmp/door.camera.watch_dog.log
 cd /home/ma/wator/starbian.wator/sample/door.camera && nodejs watch_dog.js >${log} 2>&1

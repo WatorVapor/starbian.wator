0b95e27811d52851e326b8e69aeb82b244f425de
Wator Vapor <watorvapor@gmail.com>
2018-10-27 15:29:12 +0900

    Update main.cpp

--- sample/door.camera/opencv/main.cpp
+++ sample/door.camera/opencv/main.cpp
@@ -17,6 +17,86 @@ using namespace std;
 const string cascade_name("/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml");
 
 
+#include <boost/asio/ip/address.hpp>
+#include <redisclient/redisasyncclient.h>
+class RedisEntryClient
+{
+public:
+    RedisEntryClient(boost::asio::io_service &ioService)
+        : ioService(ioService)
+    {}
+    void onMessageAPI(const std::vector<char> &buf);
+private:
+    boost::asio::io_service &ioService;
+};
+
+const string strConstDoorImageChannelName("door.camera.image");
+const string strConstDoorFaceChannelName("door.camera.face");
+
+
+void redis_sub_main(void) {
+  for(;;) {
+    try{
+      boost::asio::io_service ioService;
+      boost::asio::ip::tcp::resolver resolver(ioService);
+      boost::asio::ip::tcp::resolver::query query("127.0.0.1", "6379");
+      boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
+      boost::asio::ip::tcp::endpoint endpoint = iter->endpoint();
+      DUMP_VAR(endpoint);
+      RedisEntryClient client(ioService);
+      redisclient::RedisAsyncClient subscriber(ioService);
+      subscriber.connect(endpoint, [&](boost::system::error_code ec){
+        if(ec) {
+          DUMP_VAR(ec);
+        } else {
+          DUMP_VAR(ec);
+          subscriber.subscribe(strConstDoorImageChannelName,std::bind(&RedisEntryClient::onMessageAPI, &client, std::placeholders::_1));
+        }
+      });
+      ioService.run();
+    } catch(std::exception e) {
+      DUMP_VAR(e.what());
+    }
+    std::this_thread::sleep_for(10s);
+  }
+}
+
+void RedisEntryClient::onMessageAPI(const std::vector<char> &buf) {
+  string msg(buf.begin(),buf.end());
+  DUMP_VAR(msg);  
+}
+
+static std::weak_ptr<redisclient::RedisAsyncClient> gPublishRef;
+
+void redis_pub_main(void) {
+  for(;;) {
+    try {
+      boost::asio::io_service ioService;
+      boost::asio::ip::tcp::resolver resolver(ioService);
+      boost::asio::ip::tcp::resolver::query query("127.0.0.1", "6379");
+      boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
+      boost::asio::ip::tcp::endpoint endpoint = iter->endpoint();
+      DUMP_VAR(endpoint);
+      RedisEntryClient client(ioService);
+      auto publish = std::make_shared<redisclient::RedisAsyncClient>(ioService);
+      DUMP_VAR(publish);
+      gPublishRef = publish;
+      publish->connect(endpoint, [&](boost::system::error_code ec) {
+        if(ec) {
+          DUMP_VAR(ec);
+        } else {
+          DUMP_VAR(ec);
+        }
+      });
+      DUMP_VAR(publish);
+      ioService.run();
+    } catch(std::exception e) {
+      DUMP_VAR(e.what());
+    }
+    std::this_thread::sleep_for(10s);
+  }
+}
+
 int main (int argc, char **argv) {
   
   if( argc < 2){

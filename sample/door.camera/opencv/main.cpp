0f6857bf3f9b184029649fe8909ccd6e261ab539
Wator Vapor <watorvapor@gmail.com>
2018-10-27 14:32:45 +0900

    Update main.cpp

--- sample/door.camera/opencv/main.cpp
+++ sample/door.camera/opencv/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define DUMP_VAR(x)                                                            \
@@ -9,11 +10,35 @@ using namespace std;
   }
 
 #include "opencv2/opencv.hpp"
+#include "opencv2/objdetect.hpp" 
 
 const string cascade_name("/opt/local/share/opencv/haarcascades/haarcascade_frontalface_default.xml");
 
+
 int main (int argc, char **argv) {
-  cv::VideoCapture cap(0);
-  DUMP_VAR(cap.isOpened());
+/*  
+  if( argc < 2){
+    DUMP_VAR(argc);
+    DUMP_VAR(argv[0]);
+    return 0;
+  }
+  string fileName = string(argv[1];
+*/
+  string fileName = "/tmp/door_camera/image/image.1540617849915.png";
+  DUMP_VAR(fileName);
+  cv::Mat image = cv::imread(fileName, CV_LOAD_IMAGE_COLOR);
+  if(! image.data ) {
+    DUMP_VAR(fileName);
+    return 0;
+  }
+  cv::CascadeClassifier cascade;
+  cascade.load( cascade_name ) ;
+
+  cv::Mat gray;
+  cv::cvtColor( image, gray, cv::COLOR_BGR2GRAY );
+  vector<cv::Rect> faces;
+  cascade.detectMultiScale( gray, faces, 1.1,  
+                            2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30) );
+  DUMP_VAR(faces.size());
   return 0;
 }

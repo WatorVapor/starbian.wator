d40bb7b907320e47dcb295237b0accf745f3e621
Wator Vapor <watorvapor@gmail.com>
2019-11-14 20:52:18 +0900

    Update run.sh

--- ws/run.sh
+++ ws/run.sh
@@ -2,4 +2,5 @@
 ./install_update.sh
 export NODE_PATH=/usr/local/lib/node_modules:/usr/src/app/node_modules:$NODE_PATH
 #node --expose-gc ./starbianWSApp.js || true
+node -v || true
 node ./starbianWSApp.js || true

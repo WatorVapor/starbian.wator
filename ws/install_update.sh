6c13f793eeef5e726bce1e1eae7e0114fb067648
Wator Vapor <watovapor@gmail.com>
2021-05-15 01:05:46 +0000

    rewrite

--- ws/install_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "start update packages"
-npm i --save
-#npm audit fix
-#npm audit
-echo "finnish update packages"
--- ws/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./install_update.sh
-export NODE_PATH=/usr/local/lib/node_modules:/usr/src/app/node_modules:$NODE_PATH
-#node --expose-gc ./starbianWSApp.js || true
-node -v || true
-node ./starbianWSApp.js || true

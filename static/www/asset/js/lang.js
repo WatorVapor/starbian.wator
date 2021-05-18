719658adcaad9733084d2f222cb8d18908f4fbd4
Wator Vapor <watovapor@gmail.com>
2021-05-18 06:59:38 +0000

    static

--- static/www/asset/js/lang.js
+++ static/www/asset/js/lang.js
@@ -1,21 +1,23 @@
 const constKeyLanguangeCode = 'lang_code';
+//const RootPath = '/starbian';
+const RootPath = '';
 document.addEventListener('DOMContentLoaded', async (evt) => {
   let lang = localStorage.getItem(constKeyLanguangeCode);
   if(!lang) {
     lang = 'cn';
   }
-  console.log('::location=<',location,'>');
+  //console.log('::location=<',location,'>');
   //console.log('::lang=<',lang,'>');
   const langURL = `${location.pathname}lang_${lang}.js`;
   //console.log('::langURL=<',langURL,'>');
   const langPromise = import(langURL);
   const langModule = await langPromise;
-  console.log('::langModule.data=<',langModule.data,'>');
-  const langCommonURL = `/layout/lang_${lang}.js`;
-  console.log('::langCommonURL=<',langCommonURL,'>');
+  //console.log('::langModule.data=<',langModule.data,'>');
+  const langCommonURL = `${RootPath}/layout/lang_${lang}.js`;
+  //console.log('::langCommonURL=<',langCommonURL,'>');
   const langCommonPromise = import(langCommonURL);
   const langCommonModule = await langCommonPromise;
-  console.log('::langCommonModule.data=<',langCommonModule.data,'>');
+  //console.log('::langCommonModule.data=<',langCommonModule.data,'>');
 
   const allData = {...langModule.data, ...langCommonModule.data}
 
@@ -30,3 +32,12 @@ document.addEventListener('DOMContentLoaded', async (evt) => {
     app.mount(el);
   });
 });
+
+
+
+window.onClickChangeLang = (lang)=> {
+  console.log('onClickChangeLang::lang=<',lang,'>');
+  localStorage.setItem(constKeyLanguangeCode,lang);
+  location.reload(true);
+};
+
